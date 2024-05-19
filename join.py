0ddf62278fed99ecfa61cc64b411a0f4239188dc
abumat1 <cryptid.ojin@gmail.com>
2024-05-20 04:03:54 +0900

    モデル変更に伴うcsv変更

--- join.py
+++ join.py
@@ -5,18 +5,18 @@ if __name__ == '__main__':
     units = lib.get_units()
     with open(const.JOINED_CSV_PATH, 'w') as f:
         print('エリア',
-            'グループ名',
-            '発電所名',
-            'ユニット名',
-            '発電方式',
-            '凡例のユニット名',
-            '認可出力(万kW)',
+              'グループ名',
+              '発電所名',
+              'ユニット名',
+              '発電方式',
+              '凡例のユニット名',
+              '認可出力(万kW)',
             sep=',', file=f)
         for unit in units:
-            print(unit.plant.group.area.name,
-                unit.plant.group.name,
-                unit.plant.key,
-                unit.key,
+            print(unit.group.area.name,
+                unit.group.name,
+                unit.plant_key_name,
+                unit.unit_key_name,
                 unit.type_.name,
                 unit.name,
                 unit.power,
